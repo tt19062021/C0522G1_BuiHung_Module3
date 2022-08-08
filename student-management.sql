017ae33a69882a41ba326d34d9623b8e7a8c845f
C0522G1-buihung <tt19062021@gmail.com>
2022-08-08 20:09:48 +0700

    csdl

--- student-management.sql
+++ student-management.sql
@@ -15,3 +15,16 @@ INSERT INTO `student-management`.`class` (`id`, `name`) VALUES ('1', 'C522G1');
 INSERT INTO `student-management`.`class` (`id`, `name`) VALUES ('2', 'C0622G1');
 INSERT INTO `student-management`.`teacher`(`id`, `name`,`age`,`country`) VALUES ('1', 'Bùi Hùng','19','Đà Nẵng');
 INSERT INTO `student-management`.`teacher`(`id`, `name`,`age`,`country`) VALUES ('2', 'Nguyễn Tất Thành','18','Đà Nẵng');
+
+select * from `student-management`.`class`;
+select * from `student-management`.`teacher`;
+
+update `student-management`.`teacher` set age = 20 where id = 2; 
+update `student-management`.`teacher` set age = 17 where id = 1; 
+
+delete from `student-management`.`teacher` where id = 2;
+
+alter table `student-management`.`teacher` add column score int;
+-- Drop database,table
+drop database `student-management`;
+
